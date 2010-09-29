afa0c1b5f2f1798177b09ee2bfaff8dcf7b8993a
Xiangfu Liu <xiangfu@sharism.cc>
2010-09-29 10:51:31 +0800

    use the original bz2 file as the orig.tar.bz2, do not repackage it, since the upstream file use bz2 as the package method.
    Signed-off-by: Xiangfu Liu <xiangfu@sharism.cc>

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -8,14 +8,7 @@ set -e
 
 : ${VERSION=201007}
 
-mkdir -p debian-orig-source
-trap 'rm -fr debian-orig-source xburst-tools_${VERSION}.tar.bz2 || exit 1' EXIT INT TERM
-
 [ -f xburst-tools_${VERSION}.tar.bz2 ] || \
 	wget http://projects.qi-hardware.com/media/upload/xburst-tools/files/xburst-tools_${VERSION}.tar.bz2
 
-tar -jxf xburst-tools_${VERSION}.tar.bz2 -C debian-orig-source
-rm -rf debian-orig-source/debian
-cd debian-orig-source && tar -czf ../../xburst-tools_${VERSION}.orig.tar.gz . && cd ..
-
-rm -fr debian-orig-source xburst-tools_${VERSION}.tar.bz2
+mv xburst-tools_${VERSION}.tar.bz2 ../xburst-tools_${VERSION}.orig.tar.bz2
