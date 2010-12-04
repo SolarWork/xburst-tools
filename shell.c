4f6670e5af2e3eea457d48d8e4888b712fdcf5ad
Peter Zotov <whitequark@whitequark.org>
2010-12-04 05:17:56 +0300

    Fix echo shell command to print all arguments.

--- shell.c
+++ shell.c
@@ -319,7 +319,16 @@ static int builtin_echo(int argc, char *argv[]) {
 		return -1;
 	}
 
-	puts(argv[1]);
+	for(int i = 1; i < argc; i++) {
+		printf("%s", argv[i]);
+
+		if(i < argc - 1) {
+			printf(" ");
+
+		} else {
+			printf("\n");
+		}
+	}
 
 	return 0;
 }
